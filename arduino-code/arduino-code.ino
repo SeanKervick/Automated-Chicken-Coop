3cfb24c13f92538725ce6ca9d1ab4e072ce49e83
SeanKervick <200104300@mail.wit.ie>
2023-12-31 13:53:57 +0000

    Switching relay and event notifications addedd.

--- arduino-code/arduino-code.ino
+++ arduino-code/arduino-code.ino
@@ -10,21 +10,17 @@
 #include "config.h"
 
 int count;
-int value = analogRead(A0);
-
 
 BlynkTimer timer;
 MKRIoTCarrier carrier;
 
 void setup() {
-
   //Debug console
   Serial.begin(9600);
   Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
 
   //Setup a function to be called every second
-  timer.setInterval(2000L, writeChickenCount);
-  timer.setInterval(2000L, writeLightLevel);
+  timer.setInterval(2000L, sendData);
   carrier.begin();
   carrier.display.fillScreen(0x07E0);
 
@@ -34,32 +30,47 @@ void setup() {
   if (!APDS.begin()) {
     Serial.println("Error initializing APDS-9960 sensor.");
     //Stop forever
-    while (true);  
+    while (true);
   }
   Serial.println("Counting Chickens...");
 }
 
 void loop() {
-
   Blynk.run();
   timer.run();
   gesture();
-  
 }
 
-//This function sends chicken count every second to Virtual Pin 1.
-void writeChickenCount() {
+void sendData() {
+  // Send chicken count to Virtual Pin 1
   Blynk.virtualWrite(V1, count);
-}
-
-//This function sends the light level from LDR connected to A0 every second to Virtual Pin 2.
-void writeLightLevel() {
+  // Read and send light level data from LDR connected to A0 to Virtual Pin 2.
   float lightLevel = analogRead(A0);
   Blynk.virtualWrite(V2, lightLevel);
+
+  // Trigger event to notify owner when 5 chickens have entereed the coop.
+  if (count > 4) {
+  Blynk.logEvent("full_coop", "There are 5 chickens in the coop. Would you like to close the gate?");
+  }
+
+  // Trigger event to notify owner when it gets dark outside.
+  if (lightLevel > 850) {
+  Blynk.logEvent("light_level", "It is dark outside, would you like to close the gate?");
+  }
+}
+
+// This function is called every time the Virtual Pin 0 state changes
+BLYNK_WRITE(V0) {
+  // Set incoming value from pin V0 to a variable
+  int relayOn = param.asInt();
+  if (relayOn) {
+    carrier.Relay1.open();
+  } else {
+    carrier.Relay1.close();
+  }
 }
 
 void gesture() {
-
   //Check if a gesture reading is available
   if (APDS.gestureAvailable()) {
     int gesture = APDS.readGesture();
