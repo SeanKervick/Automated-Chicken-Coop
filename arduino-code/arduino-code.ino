799fb1667c0f507dfb64bafc33bbc76e6dfed0c7
SeanKervick <200104300@mail.wit.ie>
2024-01-05 22:21:07 +0000

    FINAL COMMIT

--- arduino-code/arduino-code.ino
+++ arduino-code/arduino-code.ino
@@ -2,40 +2,31 @@
 #define BLYNK_TEMPLATE_NAME "Insert_Here"
 #define BLYNK_AUTH_TOKEN "Insert_Here"
 
+// Required libraries
 #include <SPI.h>
 #include <WiFi.h>
 #include <BlynkSimpleWifi.h>
 #include <Arduino_MKRIoTCarrier.h>
-#include <Arduino_APDS9960.h>
-#include "SR04.h" // Add the SR04 library for the ultrasonic sensor
+#include <Arduino_APDS9960.h> // Gesture sensor.
 #include "config.h"
 
-int count;
+int count; // Chicken counter
+int proximity; // Proximity sensor (Gate status)
+int isGateClosed = 0; // 0 for open 1 for closed.
+
 BlynkTimer timer;
 MKRIoTCarrier carrier;
 
-// Ultrasonic sensor
-int trigPin = 9;    // Trigger
-int echoPin = 10;   // Echo
-SR04 sr04 = SR04(echoPin, trigPin);
 
 void setup() {
-  // Define inputs and outputs
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-
   // Debug console
   Serial.begin(9600);
   Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
-
   // Setup a function to be called every second
   timer.setInterval(2000L, sendData);
   carrier.begin();
-
-
   // Wait for Serial Monitor to open
   while (!Serial);
-
   // Initialize gesture sensor
   if (!APDS.begin()) {
     Serial.println("Error initializing APDS-9960 sensor.");
@@ -48,58 +39,13 @@ void setup() {
 void loop() {
   Blynk.run();
   timer.run();
-  gesture();
+  countChickens();
   eventTrigger();
-}
-
-
-// This function uses an ultrasonic sensor to monitor the status of the gate and only allows events to be triggered when the gate is open. (cmValue > 22)
-void eventTrigger() {
-  long cmValue = sr04.Distance();
-  Serial.print("distance = ");
-  Serial.println(cmValue);
-
-  // Check the condition without using a while loop
-  if (cmValue > 22) {
-    // Trigger event to notify owner when 5 chickens have entered the coop.
-    if (count > 4) {
-      Blynk.logEvent("full_coop", "There are 5 chickens in the coop. Would you like to close the gate?");
-    }
-
-    // Trigger event to notify owner when it gets dark outside.
-    float lightLevel = analogRead(A0);
-    if (lightLevel > 850) {
-      Blynk.logEvent("light_level", "It is dark outside, would you like to close the gate?");
-    }
-  }
-}
-
-// This function send the data to Blynk, it is called in the setup every 2 seconds.
-void sendData() {
-  // Send chicken count to Virtual Pin 1
-  Blynk.virtualWrite(V1, count);
-
-  // Read and send light level data from LDR connected to A0 to Virtual Pin 2.
-  float lightLevel = analogRead(A0);
-  Blynk.virtualWrite(V2, lightLevel);
-}
-
-// This function is called when the state of the virtual pin V0 changes on the Blynk app.
-// It is used to turn on a relay for 5 seconds when the button is pressed, simulating a motor closing the gate.
-BLYNK_WRITE(V0) {
-  // Set incoming value from pin V0 to a variable
-  int relayOn = param.asInt();
-  // Relay turns on for 5 seconds when the switch is pressed, simulating a motor closing the gate.
-  if (relayOn) {
-    carrier.Relay1.open();
-    delay(5000);
-    carrier.Relay1.close();
-  } 
 }
 
 // This function uses the gesture sensor to count chickens walking in and out of the run.
 // The carrier display flashes green or red depending on which side it detects movement from.
-void gesture() {
+void countChickens() {
   // Check if a gesture reading is available
   if (APDS.gestureAvailable()) {
     int gesture = APDS.readGesture();
@@ -107,6 +53,7 @@ void gesture() {
       case GESTURE_LEFT:
         Serial.println("Detected LEFT gesture");
         count++;
+        Serial.print("Chicken count = ");
         Serial.println(count);
         carrier.display.fillScreen(0x07E0);
         delay(300);
@@ -116,6 +63,7 @@ void gesture() {
       case GESTURE_RIGHT:
         Serial.println("Detected RIGHT gesture");
         count--;
+        Serial.print("Chicken count = ");
         Serial.println(count);
         carrier.display.fillScreen(0xF800);
         delay(300);
@@ -126,5 +74,81 @@ void gesture() {
         // Ignore
         break;
     }
+  }
+}
+
+// This function uses the proximity sensor to monitor the status of the gate and only allows events to be triggered when the gate is open. (proximity !=0)
+void eventTrigger() {
+  // check if a proximity reading is available
+  if (APDS.proximityAvailable()) {
+    // read the proximity
+    int proximity = APDS.readProximity();
+    delay(50);
+    // print value to the Serial Monitor
+    Serial.print("Proximtity = ");
+    Serial.println(proximity);
+    Serial.print("Gate Status = ");
+    Serial.println(isGateClosed);
+
+    // When the gate is open
+    if (proximity != 0) {
+      isGateClosed = 0; // Status of the gate is assigned.
+
+      // Trigger event to notify owner when all chickens have entered the coop.
+      if (count > 4) {
+        Blynk.logEvent("full_coop", "There are 5 chickens in the coop. Would you like to close the gate?");
+      }
+
+      // Trigger event to notify owner when it gets dark outside but coop is not full.
+      float lightLevel = analogRead(A0);
+      // Check light level and chicken count.
+      if (lightLevel > 850 && count < 5) {
+        Blynk.logEvent("light_level", "It is dark outside, but all chickens are not accounted for!");
+      }
+
+      // Automatically close gate if all hens are in and it is dark.
+      if (count > 4 && lightLevel > 850) {
+        // Trigger the relay for 5 seconds, simulating a motor closing the gate.
+        carrier.Relay1.open();
+        delay(5000);
+        carrier.Relay1.close();
+    
+        // Log an event indicating that the gate is being closed.
+        Blynk.logEvent("auto_gate", "Closing the gate automatically.");
+      }
+      // When the gate is closed.
+    } else {
+      Serial.println("THE GATE IS CLOSED");
+      delay(1000);
+      isGateClosed = 1; //Status of the gate is assigned.
+      Serial.print("Gate Status = ");
+      Serial.println(isGateClosed);
+    }
+  }
+}
+
+// This function sends the data from the Arduino to Blynk, it is called every 2 seconds. The timer is set in the setup.
+void sendData() {
+  // Send chicken count to Virtual Pin 1
+  Blynk.virtualWrite(V1, count);
+
+  // Read and send light level data from LDR connected to A0 to Virtual Pin 2.
+  float lightLevel = analogRead(A0);
+  Blynk.virtualWrite(V2, lightLevel);
+
+  // Send gate status to Virtual Pin 3
+  Blynk.virtualWrite(V3, isGateClosed);
+  
+}
+
+// This function is called when the state of the virtual pin V0 changes on the Blynk app.
+// It is used to turn on a relay for 5 seconds when the button is pressed, simulating a motor closing the gate.
+BLYNK_WRITE(V0) {
+  // Set incoming value from pin V0 to a variable
+  int relayOn = param.asInt();
+  if (relayOn) {
+    carrier.Relay1.open();
+    delay(5000);
+    carrier.Relay1.close();
   }
 }
